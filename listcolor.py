485d714225dc517321b95fb1f068b30d394f16ac
abhijithbaiju <153052186+abhijithbaiju@users.noreply.github.com>
2025-09-17 18:58:00 +0530

    Delete listcolor.py

--- listcolor.py
+++
@@ -1,6 +0,0 @@
-list1=input("Enter first list of colors(comma separated): ").split(",")
-list2=input("Enter second list of colors(comma separated): ").split(",")
-list1=[c.strip() for c in list1]
-list2=[c.strip() for c in list2]
-diff_colors=[c for c in list1 if c not in list2]
-print("print colors in list1 but not in list2: ",diff_colors)
